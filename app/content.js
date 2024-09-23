246db446ee50464aa8185e9bb0260c0988dadc50
aye <aye1414@163.com>
2024-09-23 08:51:19 +0000

    perf: 优化更改背景颜色和字体颜色

--- app/content.js
+++ app/content.js
@@ -4,25 +4,55 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       const { bgColor, color } = request.data;
       changeBgColor(bgColor, color);
       break;
+    case 'initPage':
+      initPage();
+      break;
     default:
       break;
   }
 });
 
+
+const animation = `transition: background-color 0.5s, color 0.5s;`;
+const customStyleIdName = 'jsCustomHubitStyle';
+
 function changeBgColor(bgColor, color) {
-  if (bgColor) {
-    document.body.style.backgroundColor = bgColor;
-    const containerEle = document.getElementById('container');
-    if (containerEle) {
-      containerEle.style.backgroundColor = bgColor;
-    }
-  }
-
+  let styleInnnerHTML = '';
   if (color) {
-    const allElements = document.querySelectorAll('*');
+    styleInnnerHTML = `
+      body, * {
+        background-color: ${bgColor} !important;
+        ${animation}
+        color: ${color} !important;
+      }
+    `;
+  } else {
+    styleInnnerHTML = `
+      body, * {
+        ${animation}
+        background-color: ${bgColor} !important;
+      }
+    `;
+  }
+  handleCreateStyle(styleInnnerHTML);
+}
 
-    allElements.forEach(element => {
-        element.style.color = color;
-    });
+function initPage() {
+  handleCreateStyle(`
+    body, * {
+      ${animation}
+    }
+  `);
+}
+
+function handleCreateStyle(styleInnnerHTML) {
+  const hasStyle = document.getElementById(customStyleIdName);
+  if (hasStyle) {
+    hasStyle.innerHTML = styleInnnerHTML
+  } else {
+    const style = document.createElement('style');
+    document.id = customStyleIdName;
+    style.innerHTML = styleInnnerHTML;
+    document.head.appendChild(style);
   }
 }
